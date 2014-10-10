8bff75891083fe6e1c4c61a6ec7bb58e6f0fc81b
Todd DeLand <todd.deland@gmail.com>
2014-10-10 07:47:47 -0400

    Use current location and map position to get points

--- app/src/main/java/com/mynameistodd/local/MapsFragment.java
+++ app/src/main/java/com/mynameistodd/local/MapsFragment.java
@@ -1,21 +1,34 @@
 package com.mynameistodd.local;
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.app.Fragment;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseGeoPoint;
 import com.parse.ParseQuery;
 
 import java.util.HashMap;
@@ -23,10 +36,14 @@ import java.util.List;
 
 //import android.support.v4.app.FragmentActivity;
 
-public class MapsFragment extends Fragment {
+public class MapsFragment extends Fragment implements
+        GooglePlayServicesClient.ConnectionCallbacks,
+        GooglePlayServicesClient.OnConnectionFailedListener {
 
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
     private HashMap<Marker, String> mMarkers;
+    private LocationClient mLocationClient;
 
     public MapsFragment() {
     }
@@ -34,6 +51,16 @@ public class MapsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mMarkers = new HashMap<Marker, String>();
+        mLocationClient = new LocationClient(getActivity(), this, this);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (servicesConnected()) {
+            mLocationClient.connect();
+        }
     }
 
     @Override
@@ -60,15 +87,23 @@ public class MapsFragment extends Fragment {
         }
     }
 
+    @Override
+    public void onStop() {
+        if (mLocationClient.isConnected()) {
+            mLocationClient.disconnect();
+        }
+        super.onStop();
+    }
+
     /**
      * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
      * installed) and the map has not already been instantiated.. This will ensure that we only ever
      * call {@link #setUpMap()} once when {@link #mMap} is not null.
-     * <p>
+     * <p/>
      * If it isn't installed {@link SupportMapFragment} (and
      * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
      * install/update the Google Play services APK on their device.
-     * <p>
+     * <p/>
      * A user can return to this FragmentActivity after following the prompt and correctly
      * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
      * have been completely destroyed during this process (it is likely that it would only be
@@ -91,29 +126,11 @@ public class MapsFragment extends Fragment {
     /**
      * This is where we can add markers or lines, add listeners or move the camera. In this case, we
      * just add a marker near Africa.
-     * <p>
+     * <p/>
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        LatLng temp = new LatLng(42.279923, -83.749996); //need to put real current location here.
-        mMarkers = new HashMap<Marker, String>();
-
-        ParseQuery<Business> query = ParseQuery.getQuery(Business.class);
-        query.findInBackground(new FindCallback<Business>() {
-            @Override
-            public void done(List<Business> businesses, ParseException e) {
-                for (Business business : businesses) {
-                    Marker marker = mMap.addMarker(new MarkerOptions()
-                            .position(new LatLng(business.getLocation().getLatitude(), business.getLocation().getLongitude()))
-                            .title(business.getName())
-                            .snippet(business.getSnippet()));
-                    mMarkers.put(marker, business.getObjectId());
-                }
-            }
-        });
-
         mMap.setMyLocationEnabled(true);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(temp, 15));
         mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
             @Override
             public boolean onMarkerClick(Marker marker) {
@@ -134,5 +151,150 @@ public class MapsFragment extends Fragment {
                 subscribeDialogFragment.show(getFragmentManager(), "SubscribeDialogFragment");
             }
         });
+        mMap.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener() {
+            @Override
+            public void onCameraChange(CameraPosition cameraPosition) {
+                updateMap(cameraPosition.target);
+            }
+        });
+    }
+
+    private void updateMap(LatLng latLng) {
+        mMarkers.clear();
+        mMap.clear();
+        ParseGeoPoint parseGeoPoint = new ParseGeoPoint(latLng.latitude, latLng.longitude);
+
+        ParseQuery<Business> query = ParseQuery.getQuery(Business.class);
+        //query.whereNear("location", parseGeoPoint);
+        query.whereWithinMiles("location", parseGeoPoint, 1.0); //TODO Distance needs to be a preference
+        query.findInBackground(new FindCallback<Business>() {
+            @Override
+            public void done(List<Business> businesses, ParseException e) {
+                for (Business business : businesses) {
+                    Marker marker = mMap.addMarker(new MarkerOptions()
+                            .position(new LatLng(business.getLocation().getLatitude(), business.getLocation().getLongitude()))
+                            .title(business.getName())
+                            .snippet(business.getSnippet()));
+                    mMarkers.put(marker, business.getObjectId());
+                }
+            }
+        });
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case CONNECTION_FAILURE_RESOLUTION_REQUEST:
+                switch (resultCode) {
+                    case Activity.RESULT_OK:
+                        //try to get location again and update map.
+                        break;
+                }
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Location location = mLocationClient.getLastLocation();
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 15));
+
+        //mLocationClient.disconnect();
+    }
+
+    @Override
+    public void onDisconnected() {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(
+                        getActivity(),
+                        CONNECTION_FAILURE_RESOLUTION_REQUEST);
+                /*
+                 * Thrown if Google Play services canceled the original
+                 * PendingIntent
+                 */
+            } catch (IntentSender.SendIntentException e) {
+                // Log the error
+                e.printStackTrace();
+            }
+        } else {
+            // Get the error code
+            int errorCode = connectionResult.getErrorCode();
+            // Get the error dialog from Google Play services
+            Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(
+                    errorCode,
+                    getActivity(),
+                    CONNECTION_FAILURE_RESOLUTION_REQUEST);
+            // If Google Play services can provide an error dialog
+            if (errorDialog != null) {
+                // Create a new DialogFragment for the error dialog
+                ErrorDialogFragment errorFragment =
+                        new ErrorDialogFragment();
+                // Set the dialog in the DialogFragment
+                errorFragment.setDialog(errorDialog);
+                // Show the error dialog in the DialogFragment
+                errorFragment.show(
+                        getFragmentManager(),
+                        "CurrentLocationFailed");
+            }
+        }
+    }
+
+    private boolean servicesConnected() {
+        // Check that Google Play services is available
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getActivity());
+        // If Google Play services is available
+        if (ConnectionResult.SUCCESS == resultCode) {
+            // In debug mode, log the status
+            Log.d("Location Updates", "Google Play services is available.");
+            // Continue
+            return true;
+            // Google Play services was not available for some reason.
+            // resultCode holds the error code.
+        } else {
+            // Get the error dialog from Google Play services
+            Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(
+                    resultCode,
+                    getActivity(),
+                    CONNECTION_FAILURE_RESOLUTION_REQUEST);
+
+            // If Google Play services can provide an error dialog
+            if (errorDialog != null) {
+                // Create a new DialogFragment for the error dialog
+                ErrorDialogFragment errorFragment = new ErrorDialogFragment();
+                // Set the dialog in the DialogFragment
+                errorFragment.setDialog(errorDialog);
+                // Show the error dialog in the DialogFragment
+                errorFragment.show(getFragmentManager(), "Location Updates");
+            }
+            return false;
+        }
+    }
+
+    public static class ErrorDialogFragment extends DialogFragment {
+        // Global field to contain the error dialog
+        private Dialog mDialog;
+
+        // Default constructor. Sets the dialog field to null
+        public ErrorDialogFragment() {
+            super();
+            mDialog = null;
+        }
+
+        // Set the dialog to display
+        public void setDialog(Dialog dialog) {
+            mDialog = dialog;
+        }
+
+        // Return a Dialog to the DialogFragment.
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            return mDialog;
+        }
     }
 }
