c280004fac1c47b8c1afc40200f3ee4c48580ffd
Max Nesler <maxnesler@gmail.com>
2014-12-30 10:51:16 -0500

    Detail image added, hard coded to brazil, but meh...

--- iOS/Local/DetailViewController.h
+++ iOS/Local/DetailViewController.h
@@ -10,7 +10,8 @@
 #import "Business.h"
 @interface DetailViewController : UIViewController
 @property (weak, nonatomic) IBOutlet UILabel *label;
-@property (weak, nonatomic) IBOutlet UIButton *button;
+@property (weak, nonatomic) IBOutlet UILabel *snippet;
+@property (strong, nonatomic) IBOutlet UIImageView *staticImage;
 @property Business *business;
 
 @end
--- iOS/Local/DetailViewController.m
+++ iOS/Local/DetailViewController.m
@@ -22,8 +22,18 @@
     Business *b = self.business;
     //self.button.titleLabel.text = @"fuck the police";
     self.label.text = [b objectForKey:@"name"];
+    self.snippet.text = [b objectForKey:@"snippet"];
     
-    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSString *imgURL = @"https://maps.googleapis.com/maps/api/staticmap?center=-15.800513,-47.91378&zoom=11&size=200x200";
+        NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:imgURL]];
+        
+        //set your image on main thread.
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self.staticImage setImage:[UIImage imageWithData:data]];
+            
+        });    
+    });
     
 }
 
