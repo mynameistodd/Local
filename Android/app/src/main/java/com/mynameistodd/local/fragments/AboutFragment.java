4f1808e3280986af37c2b8630d3bba7f7e117c0d
Todd DeLand <todd.deland@gmail.com>
2015-05-04 07:40:12 -0400

    Add versions to about screen, make gradle build smarter

--- Android/app/src/main/java/com/mynameistodd/local/fragments/AboutFragment.java
+++ Android/app/src/main/java/com/mynameistodd/local/fragments/AboutFragment.java
@@ -8,9 +8,11 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.google.android.gms.analytics.HitBuilders;
 import com.google.android.gms.analytics.Tracker;
+import com.mynameistodd.local.BuildConfig;
 import com.mynameistodd.local.LocalApplication;
 import com.mynameistodd.local.R;
 
@@ -79,7 +81,10 @@ public class AboutFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        View v = inflater.inflate(R.layout.fragment_about, container, false);
+        TextView version = (TextView) v.findViewById(R.id.about_version);
+        version.setText("Version " + BuildConfig.VERSION_NAME);
+        return v;
     }
 
     @Override
